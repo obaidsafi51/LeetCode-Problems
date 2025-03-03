3c0767e0cdeeb252028283a02470078815508c92
Obaid Ullah <134671014+obaidsafi51@users.noreply.github.com>
2025-03-03 10:00:35 +0500

    Time: 34 ms (52.23%), Space: 34.5 MB (57.71%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        less = [num for num in nums if num < pivot]
+        equal = [num for num in nums if num == pivot]
+        greater = [num for num in nums if num > pivot]
+
+        result = less + equal + greater
+        return result
