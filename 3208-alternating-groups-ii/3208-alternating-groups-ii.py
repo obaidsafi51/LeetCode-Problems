b1a9c5ee60dd192309ee6a2e4cd5e6d8f5922877
Obaid Ullah <134671014+obaidsafi51@users.noreply.github.com>
2025-03-09 15:55:19 +0500

    Time: 659 ms (98.67%), Space: 21.1 MB (90.7%) - LeetHub

--- 3208-alternating-groups-ii/3208-alternating-groups-ii.py
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.py
@@ -1,44 +1,20 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        l = []
         n = len(colors)
-    
-        # Edge case: if k == 1, every single tile by itself is trivially alternating.
-        # So there are n groups (the entire circle).
-        if k == 1:
-            return n
-        
-        # If k > n, there can't be a contiguous block of length k in a circle of length n
-        # (unless the problem statement implies we can't have k>n, but just in case):
-        if k > n:
-            return 0
-        
-        # 1) Double the circle
-        extended = colors + colors  # length = 2n
-        
-        # 2) Build the 'alt' array: alt[i] = 1 if extended[i] != extended[i+1], else 0
-        alt = [0] * (2*n - 1)
-        for i in range(2*n - 1):
-            alt[i] = 1 if extended[i] != extended[i+1] else 0
-        
-        # 3) Prefix sum of alt
-        prefixAlt = [0] * (2*n)
-        for i in range(2*n - 1):
-            prefixAlt[i+1] = prefixAlt[i] + alt[i]
-        # Note: prefixAlt[-1] is prefix sum up to the last element of alt
-        
-        # 4) Count how many starting indices [0..n-1] yield an alternating block of length k
-        count = 0
-        for start in range(n):
-            end = start + (k - 1)  # this is where we check alt subarray
-            if end < 2*n:  # must be within bounds
-                # Sum of alt[start.. end-1] = prefixAlt[end] - prefixAlt[start]
-                # We want this sum == (k - 1)
-                if prefixAlt[end] - prefixAlt[start] == (k - 1):
-                    count += 1
-            else:
-                # If end >= 2n, we can't form a length-k window in the extended array
-                # from this start. So break out early.
-                break
-        return count
+        for i in range(n - 1):
+            if colors[i] == colors[i + 1]:
+                l.append(i)
 
-        
+        if colors[n - 1] == colors[0]:
+            l.append(n - 1)
+
+        if len(l) == 0:
+            return n
+
+        l.append(l[0] + n)
+        res = 0
+        for i in range(len(l) - 1):
+            res += max(0, l[i + 1] - l[i] - k + 1)
+
+        return res
